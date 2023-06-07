0e0b6a16d08cdebbca902f7d955ea8caa418ee23
luckychacha <358140284@qq.com>
2023-06-07 18:01:19 +0800

    add a atomic usize example.

--- examples/2_atomic.rs
+++ examples/2_atomic.rs
@@ -2,13 +2,16 @@ use std::{sync::atomic::AtomicUsize, thread, time::Duration};
 
 fn main() {
     let num_done = AtomicUsize::new(0);
+    let main_thread = thread::current();
 
     thread::scope(|s| {
         s.spawn(|| {
             for i in 0..100 {
-                thread::sleep(Duration::from_secs(1));
+                thread::sleep(Duration::from_micros(i * 1000));
+                // thread::sleep(Duration::from_secs(1));
 
-                num_done.store(i + 1, std::sync::atomic::Ordering::Relaxed);
+                num_done.store(i as usize + 1, std::sync::atomic::Ordering::Relaxed);
+                main_thread.unpark();
             }
         });
 
@@ -18,7 +21,9 @@ fn main() {
                 break;
             }
             println!("Working...{current_num} / 100 done.");
-            thread::sleep(Duration::from_secs(1));
+            // thread::sleep(Duration::from_secs(1));
+            // thread will be blocked during `park_timeout`, and will be awake when unpark or each duration reached.
+            thread::park_timeout(Duration::from_secs(1));
         }
     });
 
