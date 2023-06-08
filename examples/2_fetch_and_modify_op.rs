5c232ea37ddeb7e9f4c89afc1bc39d00fbe3429c
luckychacha <358140284@qq.com>
2023-06-08 11:42:20 +0800

    2: fetch and modify.

--- examples/2_fetch_and_modify_op.rs
+++ examples/2_fetch_and_modify_op.rs
@@ -1,5 +1,11 @@
-use std::sync::atomic::{AtomicI32, Ordering};
-fn main() {
+use std::{
+    sync::atomic::{AtomicI32, Ordering},
+    thread,
+    time::Duration,
+};
+
+#[allow(dead_code)]
+fn basic_op() {
     let atomic_a = AtomicI32::new(0);
 
     // add and return previous value.
@@ -11,4 +17,35 @@ fn main() {
     assert_eq!(tmp_1, 0);
     assert_eq!(tmp_2, 1);
     assert_eq!(atomic_a.load(Ordering::Relaxed), 100);
+}
+
+fn main() {
+    let total_progress = &AtomicI32::new(0);
+
+    thread::scope(|s| {
+        for thread_id in 0..4 {
+            s.spawn(move || {
+                for task_id in 0..25 {
+                    thread::sleep(Duration::from_millis(thread_id * 2500 + task_id));
+                    let previous = total_progress.fetch_add(1, Ordering::Relaxed);
+                    println!(
+                        "{:?}: previous: {previous} after: {}",
+                        thread::current().id(),
+                        previous + 1
+                    );
+                }
+            });
+        }
+
+        loop {
+            let tmp_progress = total_progress.load(Ordering::Relaxed);
+            if tmp_progress == 100 {
+                break;
+            }
+            println!("Working.. {tmp_progress} / 100");
+            thread::sleep(Duration::from_secs(1))
+        }
+    });
+
+    println!("Done.");
 }
