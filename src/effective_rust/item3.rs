3f318fe5a67a61d9a9ef0723a12a7f9c4c19fce7
luckychacha <358140284@qq.com>
2023-06-12 11:10:48 +0800

    deal unicode string into String.

--- src/effective_rust/item3.rs
+++ src/effective_rust/item3.rs
@@ -25,5 +25,32 @@ mod tests {
     fn encrypt() {
         let i = InputData { payload: None };
         println!("i: {:?}", i.encrypted());
+
+        // let unicode_string = "\u{534e}\u{90a6}\u{5065}\u{5eb7}\u{5fb7}\u{8c6a}";
+
+        // let utf8_bytes: Vec<u8> = unicode_string
+        //     .chars()
+        //     .flat_map(|c| c.encode_utf8(&mut [0; 3]).bytes().collect::<Vec<_>>())
+        //     .collect();
+
+        // println!("{:?}", utf8_bytes);
+
+        // let unicode_string = "\u{534e}\u{90a6}\u{5065}\u{5eb7} \u{5fb7}\u{8c6a}";
+
+        // if let Ok(utf8_string) = String::from_utf8(utf8_bytes) {
+        //     println!("{}", utf8_string);
+        // } else {
+        //     println!("Invalid UTF-8 sequence");
+        // }
+
+        let unicode_string = "\u{534e}\u{90a6}\u{5065}\u{5eb7}\u{5fb7}\u{8c6a}";
+
+        let utf8_bytes = unicode_string.as_bytes();
+
+        println!("&[u8]: {:?}", utf8_bytes);
+
+        let utf8_string = String::from_utf8_lossy(utf8_bytes);
+
+        println!("utf8_string: {}", utf8_string);
     }
 }
