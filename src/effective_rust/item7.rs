c22f1acef8f784d0311b2e1ded7cfde1e950c85e
luckychacha <358140284@qq.com>
2023-06-18 00:31:49 +0800

    item7: newtype.

--- src/effective_rust/item7.rs
+++ src/effective_rust/item7.rs
@@ -1,8 +1,13 @@
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub struct NewType(usize);
 
 use std::fmt;
 impl fmt::Display for NewType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        self.0.fmt(f)
+    }
+}
+impl fmt::Debug for NewType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         self.0.fmt(f)
     }
