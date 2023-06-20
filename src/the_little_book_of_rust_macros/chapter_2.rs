b3b300fb95880d828fa9d1fe68d0dbc642a1739e
luckychacha <358140284@qq.com>
2023-06-21 00:53:43 +0800

    add some comment about PartialEq, Deref Trait and assert_eq! macro.

--- src/the_little_book_of_rust_macros/chapter_2.rs
+++ src/the_little_book_of_rust_macros/chapter_2.rs
@@ -66,11 +66,22 @@ mod tests {
 
     #[test]
     fn vec_strs_should_work() {
+        PartialEq
         let v = vec_strs!("a", "b", "c");
         // error[E0277]: can't compare `&[&str; 3]` with `Vec<String>`
         // 因为 assert_eq! 源码中是 *left == *right 进行比较 所以左侧需要实现对右侧的 PartialEq Trait
+        // 切片的 PartialEq 要求的是：
+        // impl<A, B> PartialEq<[B]> for [A]
+        // where A: PartialEq<B>
+        // 所以首先需要切片和切片才可以比较
+        // 其次，左侧需要实现针对 B 类型的PartialEq Trait
+        // &str 实现了 PartialEq<String>，因为 String 实现了 Deref Trait，可以转换成 &str
+        // 于是这个实现可以让二者进行比较：https://doc.rust-lang.org/stable/src/core/str/traits.rs.html#27
+
+        // 1.将 Vec<String> 转换为切片(assert_eq!(&["a", "b", "c"], v.as_slice());)可以比较，是因为 &str 实现了 PartialEq<String>。这个地方和 String 实现了 Deref Trait 有关 ，可以将 &String 变成 &str。
+        // 2.Vec<String> 可以和 &[&str] 比较，我理解也和 String 实现了 Deref Trait 有关，String解引用成&str，符合Vec<T> 实现 PartialEq 的要求，所以也不报错。
+        // assert_eq!(&["a", "b", "c"], v);
         assert_eq!(&["a", "b", "c"], v.as_slice());
-        assert_eq!(&["a", "b", "c"], v);
         assert_eq!(v, &["a", "b", "c"]);
     }
 }
