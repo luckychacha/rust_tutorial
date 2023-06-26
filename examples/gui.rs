8fd770bc688dff3c3fb8da13727e63956058eb19
luckychacha <358140284@qq.com>
2023-06-26 23:58:10 +0800

    add workspace for proc macros.

--- examples/gui.rs
+++ examples/gui.rs
@@ -1,5 +1,4 @@
-use rust_tutorial::types::gui::{button::Button, label::Label, window::Window, Widget};
-
+use tutorial::types::gui::{button::Button, label::Label, window::Window, Widget};
 fn main() {
     let mut window = Window::new("Rust GUI Demo 1.23");
     window.add_widget(Box::new(Label::new("This is a small text GUI demo.")));
