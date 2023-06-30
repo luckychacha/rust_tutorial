f6330d282394f218f0c503cc221f79ae07966e64
luckychacha <358140284@qq.com>
2023-06-30 13:07:28 +0800

    deal conflict

--- examples/test.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let width = 20;
-    println!("left aligned:  |{:/<width$}|", "foo");
-    println!("centered:      |{:/^width$}|", "foo");
-    println!("right aligned: |{:/>width$}|", "foo");
-}
--- examples/unsafe_example.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let emojis = "😄∈🌏";
-
-    unsafe {
-        println!("{}", emojis.get_unchecked(0..4));
-        println!("{}", emojis.get_unchecked(4..7));
-        println!("{}", emojis.get_unchecked(7..11));
-    }
-}
