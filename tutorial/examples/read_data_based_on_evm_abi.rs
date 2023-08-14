71a06ad1567582fcbb069a9dfb0ffa39c7f47f5a
luckychacha <358140284@qq.com>
2023-08-15 01:08:20 +0800

    Read parameter 3 successfully.

--- tutorial/examples/read_data_based_on_evm_abi.rs
+++ tutorial/examples/read_data_based_on_evm_abi.rs
@@ -52,7 +52,7 @@ fn main() {
     let parameter_1_start_offset = U256::from_big_endian(next()).low_u32() as usize;
     println!("parameter_1_start_offset: {:?}", parameter_1_start_offset);
     next();
-    let parameter_2_start_offset = U256::from_big_endian(next()).low_u32() as usize;
+    let parameter_3_start_offset = U256::from_big_endian(next()).low_u32() as usize;
 
     let parameter_1_length = U256::from_big_endian(
         &argument_encoding[parameter_1_start_offset..parameter_1_start_offset + 32],
